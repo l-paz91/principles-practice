d622bb731e6ed691935fab7478efb571b767a713
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2019-11-04 16:01:24 +0000

    Update main.cpp

--- Chapter 9/Exercise 5/main.cpp
+++ Chapter 9/Exercise 5/main.cpp
@@ -10,7 +10,21 @@
 
 int main()
 {
-	keep_window_open();
+	//create a pointer to the single instance class to prevent using Books::getInstance() every time
+	Books& booksPtr = Books::getInstance();
+
+	booksPtr.add("Lord of the Rings", "J.R.R Tolkien", "32-32-32-H", 1954);
+	booksPtr.checkOut("32-32-32-H");
+
+	//these should not be registered
+	booksPtr.add("Lord of the Things", "J.T.T Trollkien", "BH-32-32-8", 1954);
+	booksPtr.add("Lord of the DingWings", "J.T.T Trollkien", "45613-4564862-3-L", 3000);
+
+	cout << "\n# Books Registered: " << booksPtr.getBooks().size() << endl;
+	cout << "# Books Checked Out: " << booksPtr.getCBooks().size() << endl;
+
+	char ch;
+	cin >> ch;
 	return 0;
 }
 
