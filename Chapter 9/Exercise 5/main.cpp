3727181980a549360b33f6ac85be9206f5d2792e
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2019-11-22 09:36:01 +0000

    Update main.cpp

--- Chapter 9/Exercise 5/main.cpp
+++ Chapter 9/Exercise 5/main.cpp
@@ -2,29 +2,33 @@
 //
 // C9 - Exercise 5
 //
+// I struggled with this because work has me constantly thinking about data now 
+// instead of objects. Trying to just imagine 'a book' and the operations it may
+// need proved more difficult than thinking about what data a library would need
+//
 // -----------------------------------------------------------------------------
 
+//--INCLUDES--//
 #include "BookClass.h"
 
 // -----------------------------------------------------------------------------
 
 int main()
 {
-	//create a pointer to the single instance class to prevent using Books::getInstance() every time
-	Books& booksPtr = Books::getInstance();
+	Book lotr;
 
-	booksPtr.add("Lord of the Rings", "J.R.R Tolkien", "32-32-32-H", 1954);
-	booksPtr.checkOut("32-32-32-H");
+	//these should fail
+	lotr.createBook("32-32-32-jh", "Lord of the Things", "J.R.R Tolkien", 1954, false);
+	lotr.createBook("32-gg-32-X", "Lord of the DingWings", "J.R.R Tolkien", 1954, false);
+	lotr.createBook("32-32-32-X", "Lord Of The Chicken Wings", "J.R.R Tolkien", 3000, false);
+	lotr.createBook("32-32-32-X", "Lord Of The Pings", "J.R.R Tolkien", 1759, false);
 
-	//these should not be registered
-	booksPtr.add("Lord of the Things", "J.T.T Trollkien", "BH-32-32-8", 1954);
-	booksPtr.add("Lord of the DingWings", "J.T.T Trollkien", "45613-4564862-3-L", 3000);
+	//this should pass
+	lotr.createBook("32-32-32-X", "Lord Of The Rings", "J.R.R Tolkien", 1954, false);
+	cout << "Checked out: " << boolalpha << lotr.isCheckedOut() << endl;
 
-	cout << "\n# Books Registered: " << booksPtr.getBooks().size() << endl;
-	cout << "# Books Checked Out: " << booksPtr.getCBooks().size() << endl;
-
-	booksPtr.checkIn("32-32-32-H");
-	cout << "# Books Checked Out: " << booksPtr.getCBooks().size() << endl;
+	lotr.setCheckedOut(true);
+	cout << "Checked out: " << boolalpha << lotr.isCheckedOut() << endl;
 
 	char ch;
 	cin >> ch;
