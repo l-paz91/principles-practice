1aece2d06d58174f645ff1b9eb0d639ceccd3af5
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2019-11-26 11:01:14 +0000

    Update main.cpp

--- Chapter 9/Exercise 11/main.cpp
+++ Chapter 9/Exercise 11/main.cpp
@@ -11,7 +11,24 @@
 
 int main()
 {
+	using namespace Chrono;
+	
+	Date today{ 2019, Month::nov, 26 };
+	Date jan1st{ 2019, Month::jan, 1 };
 
+	cout << "Today is: " << day_of_the_week(today) << endl;
+	cout << "Jan 1st was: " << day_of_the_week(jan1st) << endl;
+
+	cout << "Days since Jan 1st: " << daysSince1stJan(today) << " (including today)" << endl;
+
+	cout << "The current week number is: " << week_of_year(today) << endl;
+
+	today.add_day(10);
+	cout << today << endl;
+	today.add_month(2);
+	cout << today << endl;
+	today.add_year(6);
+	cout << today << endl;
 
 	char ch;
 	cin >> ch;
