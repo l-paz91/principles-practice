80c22ccc7dc6d62e44ae5b2d9a763bdefde0ef7c
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2019-12-06 15:47:04 +0000

    Create MoneyClass.cpp

---
+++ Chapter 9/Exercise 14/MoneyClass.cpp
@@ -0,0 +1,62 @@
+// -----------------------------------------------------------------------------
+// MoneyClass.cpp
+// -----------------------------------------------------------------------------
+
+//--INCLUDES--//
+#include "MoneyClass.h"
+
+// -----------------------------------------------------------------------------
+
+void Money::convertToSmall(double amount)
+{
+	round(amount);		//round using 4/5 rounding rule, found in std library
+	m_money = narrow_cast<long int>(amount * 100);
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+
+std::ostream& operator<<(ostream& os, const Money& m)
+{
+	return os << "$" << fixed << setprecision(2) << m.getMoney() << endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void operator+(const Money& m1, const Money& m2)
+{
+	double m1money(m1.getMoney()), m2money(m2.getMoney());
+
+	cout << "$" << m1money << " + " << m2money << " = $" << m1money + m2money << endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void operator-(const Money& m1, const Money& m2)
+{
+	double m1money(m1.getMoney()), m2money(m2.getMoney());
+
+	cout << "$" << m1money << " - " << m2money << " = $" << m1money - m2money << endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void operator/(const Money& m1, const Money& m2)
+{
+	double m1money(m1.getMoney()), m2money(m2.getMoney());
+
+	cout << "$" << m1money << " / " << m2money << " = $" << m1money / m2money << endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void operator*(const Money& m1, const Money& m2)
+{
+	double m1money(m1.getMoney()), m2money(m2.getMoney());
+
+	cout << "$" << m1money << " * " << m2money << " = $" << m1money * m2money << endl;
+}
+
+// -----------------------------------------------------------------------------
