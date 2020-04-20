3cf5604f23ebcbe496d87c4551d189aa24169c68
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2020-04-20 13:17:37 +0100

    Update main.cpp
    reflecting changes made to Box

--- Chapter 13/Exercise 2/main.cpp
+++ Chapter 13/Exercise 2/main.cpp
@@ -4,10 +4,6 @@
 /*
    Draw a box with rounded corners. Define a class Box, consisting of four lines 
    and four arcs.
-   
-   Note - when you change the round amount, the size of the rectangle will change 
-   as the radius is being directly changed. In order to preserve rectangle size, 
-   the amount of points added along the arc need to be reduced instead.
 */
 // https://github.com/l-paz91/principles-practice/tree/master/Graphics%20Files
 // -----------------------------------------------------------------------------
@@ -24,12 +20,21 @@ int main()
 {
 	Simple_window win{ Point{100,100}, 600, 600, "Chapter 13 - Exercise 2" };
 
-	Graph_lib::Box box(Point{ 100,100 }, 300, 200, 100);
-	box.set_color(Color::black);
+	Point p(50, 50);
+	
+	Graph_lib::Box box(p, 300, 200, 20);
+	box.set_color(Color::magenta);
+	box.set_fill_color(Color::white);
 	win.attach(box);
+
+	Graph_lib::Box box2(p, 300, 200, 100);
+	box2.set_color(Color::black);
+	box2.set_fill_color(Color::green);
+	win.attach(box2);
+
 	win.wait_for_button();
 
-	box.change_roundAmount(20);
+	box2.change_roundAmount(300); //will error
 	win.wait_for_button();
 }
 
