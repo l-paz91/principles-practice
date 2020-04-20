cae9f7712ea8f5852a4e98ba075f51d153ade300
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2020-04-20 11:21:08 +0100

    Update Graph.cpp
    Fixes to Arc::draw_lines(). Now correctly fills arc portion.

--- Chapter 13/Exercise 1/Graph.cpp
+++ Chapter 13/Exercise 1/Graph.cpp
@@ -312,14 +312,14 @@ namespace Graph_lib
 		if (fill_color().visibility())
 		{	// fill
 			fl_color(fill_color().as_int());
-			fl_pie(point(0).x, point(0).y, w + w - 1, h + h - 1, 0, 360);
+			fl_pie(point(0).x, point(0).y, w, h, angle1, angle2);
 			fl_color(color().as_int());	// reset color
 		}
 
 		if (color().visibility())
 		{
 			fl_color(color().as_int());
-			fl_arc(point(0).x, point(0).y, w + w, h + h, angle1, angle2);
+			fl_arc(point(0).x, point(0).y, w, h, angle1, angle2);
 		}
 	}
 
