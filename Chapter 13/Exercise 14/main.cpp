396bba3636c8f97941e8f44eaac8ce03696bda76
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2020-05-18 21:57:39 +0100

    Create main.cpp

---
+++ Chapter 13/Exercise 14/main.cpp
@@ -0,0 +1,43 @@
+// -----------------------------------------------------------------------------
+//
+// Chapter 13 - Exercise 14
+/*
+	Define a right triangle class. Make an octagonal shape out of eight right 
+	triangles of different colours.
+*/
+// 
+// -----------------------------------------------------------------------------
+
+//--INCLUDES--//
+#include "Simple_window.h"
+#include "Graph.h"
+
+using namespace Graph_lib;
+typedef Graph_lib::RightTriangle RT;
+
+// -----------------------------------------------------------------------------
+
+int main()
+{
+	Simple_window win{ Point{100,100}, 800, 800, "Exercise 14" };
+
+	vector<RT> v_rt{
+		RT(Point(300, 0), Point(100, 200), Point(300, 400), Color::black),
+		RT(Point(100, 200), Point(100, 400), Point(300, 400), Color::blue),
+		RT(Point(100, 400), Point(300, 600), Point(300, 400), Color::cyan),
+		RT(Point(300, 0), Point(300, 600), Point(500, 0), Color::green),
+		RT(Point(300, 600), Point(500, 600), Point(500, 0), Color::red),
+		RT(Point(500, 0), Point(700, 200), Point(500, 400), Color::white),
+		RT(Point(700, 200), Point(700, 400), Point(500, 400), Color::yellow),
+		RT(Point(500, 400), Point(500, 600), Point(700, 400), Color::dark_magenta)
+	};
+
+	for (int i = 0; i < v_rt.size(); ++i)
+		win.attach(v_rt[i]);
+
+	win.wait_for_button();
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
