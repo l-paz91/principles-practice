bd78e558110932092fe35593a696f61c66b00cbb
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2021-04-07 15:23:38 +0100

    Create main.cpp

---
+++ Chapter 20/Exercise 1/tryThis p728/main.cpp
@@ -0,0 +1,35 @@
+// -----------------------------------------------------------------------------
+// https://lptcp.blogspot.com/
+// 
+/*
+	Chapter 20 - Exercise 1
+
+	If you haven't already, do all the 'Try This' exercises in the chapter.
+
+	p728 - The standard library vector doesn't provide push_front(). Why not?
+	Implement push_front for vector and compare it to push_back().
+
+*/
+// https://github.com/l-paz91/principles-practice/
+// -----------------------------------------------------------------------------
+
+//--INCLUDES--//
+#include "std_lib_facilities.h"
+
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+
+int main()
+{
+	vector<int> n{ 1,2,3,4,5,6,7,9,10 };
+	n.push_front(0);
+
+	keep_window_open();
+	return 0;
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
