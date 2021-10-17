bc3e19e721aa307a1f4c86c7e41e020ae5a8b0f7
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2021-10-17 14:32:35 +0100

    Create Order.cpp

---
+++ Chapter 21/Exercise 12/Non-GUI/Order.cpp
@@ -0,0 +1,157 @@
+// -----------------------------------------------------------------------------
+// 
+//--INCLUDES--//
+#include "Order.h"
+
+// -----------------------------------------------------------------------------
+
+Order::Order(string pName, string pAddress, time_t pDate, vector<Purchase>& pPurchases)
+	: mName(pName)
+	, mAddress(pAddress)
+	, mDate(pDate)
+	, mPurchases(pPurchases)
+{}
+
+// -----------------------------------------------------------------------------
+
+Order::Order(string pName, string pAddress, time_t pDate, std::initializer_list<Purchase> pList)
+	: mName(pName)
+	, mAddress(pAddress)
+	, mDate(pDate)
+	, mPurchases(pList)
+{}
+
+// -----------------------------------------------------------------------------
+
+Order::Order(string pFileName)
+	: Order()
+{
+	ifstream readIn{ pFileName };
+	if (!readIn)
+	{
+		cout << "Error opening: " << pFileName << endl;
+		return;
+	}
+	readInFromFile(readIn);
+}
+
+// -----------------------------------------------------------------------------
+
+Order::Order(const Order& pOrder)
+{
+	mName = pOrder.mName;
+	mAddress = pOrder.mAddress;
+	mDate = pOrder.mDate;
+	mPurchases.clear();
+	mPurchases.reserve(pOrder.mPurchases.size());
+	mPurchases = pOrder.mPurchases;
+}
+
+// -----------------------------------------------------------------------------
+
+Order& Order::operator=(const Order& order)
+{
+	mName = order.mName;
+	mAddress = order.mAddress;
+	mDate = order.mDate;
+	mPurchases.clear();
+	mPurchases.reserve(order.mPurchases.size());
+	mPurchases = order.mPurchases;
+
+	return *this;
+}
+
+// -----------------------------------------------------------------------------
+
+void Order::writeToFile()
+{
+	ofstream out(mName + " Order.txt");
+	writeOut(out);
+}
+
+// -----------------------------------------------------------------------------
+
+ostream& operator<<(ostream& outStream, const Order& o)
+{
+	return o.writeOut(outStream);
+}
+
+// -----------------------------------------------------------------------------
+
+void Order::readInFromFile(ifstream& readIn)
+{
+	if (!mName.empty())
+	{
+		cout << "This order already contains data and cannot be overwritten." << endl;
+			return;
+	}
+
+	// get name
+	getline(readIn, mName);
+	if (mName.empty())
+		return;
+
+	// get address
+	getline(readIn, mAddress);
+
+	// get date
+	string date;
+	getline(readIn, date);
+	istringstream ss(date);
+	ss.imbue(locale("en_GB.UTF-8"));
+	tm tm{};
+	ss >> get_time(&tm, "%a %b  %d %H:%M:%S %Y");
+	if (ss.fail())
+		cout << "Date parsing failed. Incorrect format." << endl;
+	else
+		mDate = mktime(&tm);
+
+	// get purchases
+	string line;
+	while (getline(readIn, line) && line != "")
+	{
+		istringstream is(line);
+		Purchase temp;
+		is >> temp;
+		mPurchases.push_back(temp);
+	}
+}
+
+// -----------------------------------------------------------------------------
+
+const double Order::getPurchaseValue() const
+{
+	double d = 0.0;
+	for (Purchase p : mPurchases)
+		d += p.getPurchaseValue();
+	return d;
+}
+
+// -----------------------------------------------------------------------------
+
+void Order::printPurchases()
+{
+	for (Purchase p : mPurchases)
+		cout << p << "\t" << p.getPurchaseValue() << endl;
+}
+
+// -----------------------------------------------------------------------------
+
+std::ostream& Order::writeOut(ostream& os) const
+{
+	os << mName << "\n"
+		<< mAddress << "\n";
+
+	string time = ctime(&mDate);
+	os << time;
+
+	for (const Purchase& p : mPurchases)
+		os << p;
+
+	os << endl;
+	return os;
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
