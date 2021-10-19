54a92d2b777be1f5e80edda5ab72a483eb8df42d
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2021-10-19 14:59:41 +0100

    Create Order.h

---
+++ Chapter 21/Exercise 12/With GUI/Order.h
@@ -0,0 +1,153 @@
+// -----------------------------------------------------------------------------
+#ifndef ORDER_H
+#define ORDER_H
+// -----------------------------------------------------------------------------
+
+//--INCLUDES--//
+#define _CRT_SECURE_NO_WARNINGS
+#include "std_lib_facilities.h"
+
+#include <ctime>
+
+// -----------------------------------------------------------------------------
+
+class Purchase
+{
+public:
+	Purchase() : mProductName(), mUnitPrice(), mQuantity() {}
+	Purchase(string pName, double pPrice, int pAmount)
+		: mProductName(pName)
+		, mUnitPrice(pPrice)
+		, mQuantity(pAmount)
+	{}
+
+	const double getPurchaseValue() const { return mUnitPrice * mQuantity; }
+
+	friend ostream& operator<<(ostream& outStream, const Purchase& p)
+	{
+		outStream << p.mProductName << "*"
+			<< p.mUnitPrice << "*"
+			<< p.mQuantity << endl;
+		return outStream;
+	}
+
+	friend istream& operator>>(istream& inStream, Purchase& p)
+	{
+		string line;
+		getline(inStream, line);
+
+		size_t star1 = line.find('*');
+		p.mProductName = line.substr(0, star1);
+
+		size_t star2 = line.find('*', star1 + 1);
+		size_t diff = star2 - (star1 + 1);
+		p.mUnitPrice = stod(line.substr(star1 + 1, diff));
+
+		size_t newline = line.find('\n', star2 + 1);
+		diff = newline - (star2 + 1);
+		p.mQuantity = stoi(line.substr(star2 + 1, diff));
+
+		return inStream;
+	}
+
+private:
+	string mProductName;
+	double mUnitPrice;
+	int mQuantity;
+};
+
+// -----------------------------------------------------------------------------
+
+class Order
+{
+public:
+	Order() : mName(), mAddress(), mDate(), mPurchases() {}
+
+	Order(string pName, string pAddress, time_t pDate, vector<Purchase>& pPurchases);
+	Order(string pName, string pAddress, time_t pDate, std::initializer_list<Purchase> pList);
+	Order(string pFileName);
+	Order(const Order& pOrder);
+
+	Order& operator=(const Order& order);
+
+	void writeToFile();
+	friend ostream& operator<<(ostream& outStream, const Order& o);
+	void readInFromFile(istream& readIn);
+	const double getPurchaseValue() const;
+	void printPurchases();
+
+	const string& getName() const { return mName; }
+	const string& getAddress() const { return mAddress; }
+
+private:
+	ostream& writeOut(ostream& os) const;
+
+	string mName;
+	string mAddress;
+	time_t mDate;
+	vector<Purchase> mPurchases;
+};
+
+// -----------------------------------------------------------------------------
+
+namespace OrderHelpers
+{
+	template<typename Container>
+	void getOrdersFromFile(Container& pCont, string pFileName)
+	{
+		ifstream readIn{ pFileName };
+		if (!readIn)
+		{
+			cout << "Error opening: " << pFileName << endl;
+			return;
+		}
+
+		while (!readIn.eof())
+		{
+			Order order;
+			order.readInFromFile(readIn);
+			pCont.insert(pCont.begin(), order);
+		}
+	}
+
+	template<typename Container>
+	double getValueOfOrders(const Container& pCont)
+	{
+		double d = 0.0;
+		for (Order o : pCont)
+		{
+			d += o.getPurchaseValue();
+			o.printPurchases();
+		}
+		return d;
+	}
+
+	struct sortByName
+	{
+		bool operator()(const Order& o1, const Order& o2)
+		{
+			return o1.getName() < o2.getName();
+		}
+	};
+
+	struct sortByAddress
+	{
+		bool operator()(const Order& o1, const Order& o2)
+		{
+			return o1.getAddress() < o2.getAddress();
+		}
+	};
+
+	inline bool searchByName(string pSearchValue, const Order& o1)
+	{
+		size_t found = o1.getName().find(pSearchValue);
+		if (found != string::npos)
+			return true;
+		return false;
+	}
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+#endif // !ORDER_H
